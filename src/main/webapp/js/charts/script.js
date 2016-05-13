c822d200e3f14332dc61b894f84301b820b0efb0
red1orchid <red1orchid@gmail.com>
2016-05-13 19:30:13 +0300

    Add files via upload

---
+++ src/main/webapp/js/charts/script.js
@@ -0,0 +1,22 @@
+$(function() {
+var tt = document.createElement('div'),
+  leftOffset = -(~~$('html').css('padding-left').replace('px', '') + ~~$('body').css('margin-left').replace('px', '')),
+  topOffset = -32;
+tt.className = 'ex-tooltip';
+document.body.appendChild(tt);
+
+var opts = {
+  "dataFormatX": function (x) { return d3.time.format('%Y-%m-%d %H:%M').parse(x); },
+  "tickFormatX": function (x) { return d3.time.format('%d.%m.%Y')(x); },
+  "mouseover": function (d, i) {
+    var pos = $(this).offset();
+    $(tt).text(d3.time.format('%d.%m.%Y')(d.x) + ': ' + d.y)
+      .css({top: topOffset + pos.top, left: pos.left + leftOffset})
+      .show();
+  },
+  "mouseout": function (x) {
+    $(tt).hide();
+  }
+};
+var myChart = new xChart('line-dotted', data, '#chart', opts);
+});
